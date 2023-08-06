a874d65ded35572076046b4285152ba58883ed3f
dmuri <72742970+dmuri@users.noreply.github.com>
2023-08-06 19:57:37 +0200

    completed project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
@@ -7,12 +8,21 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontWeight: {
+        'normal': 400,
+        'bold': 700,
+      },
+      colors: {
+        primary: {
+          darkCyan: 'hsl(185, 75%, 39%)',
+          veryDarkDesaturatedBlue: 'hsl(229, 23%, 23%)',
+          darkGrayishBlue: 'hsl(227, 10%, 46%)',
+        },
+        neutral: {
+          darkGray: 'hsl(0, 0%, 59%)',
+        },
       },
     },
-  },
-  plugins: [],
+    plugins: [],
+  }
 }
